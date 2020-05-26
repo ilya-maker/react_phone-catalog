932d9babe1c0887e7b73b9e3abca907b8bb014fa
T Z <kazakova.tanya@gmail.com>
2020-05-26 17:52:09 +0300

    test-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>React Phone Catalog</h1>
-  </div>
-);
+export const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/phones">
+          <Catalog />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
 
-export default App;
+const Header = () => <h2>Header</h2>;
+const Home = () => <h2>Home</h2>;
+const Catalog = () => <h2>Catalog</h2>;
+const Footer = () => <h2>Footer</h2>;
